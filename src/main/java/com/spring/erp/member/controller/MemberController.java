0830056fa30a8f76b35e6cdd857f9cbd32c40c6c
leechanghoon00 <ddong9124@gmail.com>
2024-08-29 18:37:25 +0900

    ViewController 생성 후 안에 Login창 띄우기 3h Controller,RestController,RequestBody,RESTful API 공부

--- src/main/java/com/spring/erp/member/controller/MemberController.java
+++ src/main/java/com/spring/erp/member/controller/MemberController.java
@@ -16,12 +16,17 @@ public class MemberController {
     }
 
 
-    @PostMapping("/api/add")
+    @PostMapping("/api/add") //회원가입
     //TODO: Restful api 방식에대해서 공부가 필요 공부후 필요한 api로 적용 할것
     public void addMember(@RequestBody MemberDTO memberDTO) {
         memberService.addMember(memberDTO);
     }
 
+
+//    @GetMapping("/login") // 로그인 창 띄우기
+//    public String showLogin() {
+//        return "login"; // login.html을 반환
+//    }
 
 //    //회원 수정
 //    @PatchMapping("/editMember")
